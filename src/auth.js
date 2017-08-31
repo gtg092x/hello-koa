8419371eb7012bce5daa83bd3073edb70b30bed2
Matthew Drake <matthew.drake@hixme.com>
2017-08-31 13:03:28 -0700

    alright

--- src/auth.js
+++ src/auth.js
@@ -6,6 +6,10 @@ export const getTokenForUser = user => jwt.sign({id: user.id}, 'secret', {
 });
 
 export const jwtMiddleware = () => async (ctx, next) => {
+  const jwtCookie = ctx.cookies.get('JWT');
+  if (!ctx.get('Authorization') && jwtCookie) {
+    ctx.request.headers['authorization'] = `Bearer ${jwtCookie}`;
+  }
   await next();
   if (ctx.state.user) {
     const token = getTokenForUser(ctx.state.user);
